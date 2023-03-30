384f638a224ab8d7f0e06b771739793a1dbdaae9
COSIMOMATTIA <102244225+COSIMOMATTIA@users.noreply.github.com>
2023-03-30 17:48:15 +0200

    Update R_code_time_series.R

--- R_code_time_series.R
+++ R_code_time_series.R
@@ -1,1 +1,7 @@
 # R code for importing and analysing several 
+library(raster)
+setwd ("C:/lab/greenland")
+lst_2000 <- raster("lst_2000.tif")
+setwd ("C:/lab/greenland")
+> lst_2000 <- raster("lst_2000.tif")
+> lst-2000
