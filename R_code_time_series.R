240a4f87b04375ae691520885778d33b705ada83
COSIMOMATTIA <102244225+COSIMOMATTIA@users.noreply.github.com>
2023-04-13 17:06:57 +0200

    Update R_code_time_series.R

--- R_code_time_series.R
+++ R_code_time_series.R
@@ -29,3 +29,38 @@ dift <- TGr[[2]] - TGr[[1]]
 plot(dift)
 cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
 plot(TGr, col=cl)
+#esercizio EN
+library(raster)
+setwd ("C:/lab/EN")
+# importo primo file
+EN_first <- raster("EN_0001.png")
+EN_first
+cl <- colorRampPalette(c('red','orange','yellow'))(100)
+plot(EN_first, col=cl)
+
+rlist <- list.files(pattern="EN")
+rlist
+import <- lapply(rlist, raster)   #per importare più file 
+import
+EN <- stack(import)
+EN
+plot(EN, col=cl)
+par(mfrow=c(1,2)) #mettere immagini l'una a fianco all'altra
+plot(EN_first, col=cl)
+plot(EN[[1]], col=cl)
+# controllo
+difcheck <- EN_first - EN[[1]]
+difcheck
+plot(difcheck)
+#primo e ultimo
+
+par(mfrow=c(1,2))
+plot(EN[[1]], col=cl)
+plot(EN[[13]], col=cl)
+
+difen = EN[[1]] - EN[[13]]
+cldif <- colorRampPalette(c('blue','white','red'))(100) 
+plot(difen, col=cldif)
+
+plotRGB(EN, 1, 7, 13, stretch="lin")
+plotRGB(EN, 1, 7, 13, stretch="hist")
