3c315a162da8a5cf60fed39f2dfaca8fd06fa887
COSIMOMATTIA <102244225+COSIMOMATTIA@users.noreply.github.com>
2023-04-13 17:48:04 +0200

    Update R_code_classification.R

--- R_code_classification.R
+++ R_code_classification.R
@@ -5,3 +5,9 @@ library(devtools)
 
 install_github("bleutner/RStoolbox")
 library(RStoolbox)
+
+library(raster)
+setwd("C:/lab/")
+
+so <- brick ("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
+plotRGB(so, 1, 2, 3, stretch="lin")
