b824563bd986e11694fb2d9c1d7df9638c6663ab
COSIMOMATTIA <102244225+COSIMOMATTIA@users.noreply.github.com>
2023-05-05 17:47:12 +0200

    Create R_code_multivariate_analysis.r

---
+++ R_code_multivariate_analysis.r
@@ -0,0 +1,62 @@
+library(raster)
+setwd("C:/lab/")
+sen <- brick("sentinel.png")
+sen
+plot(sen)
+sen2 <- stack(sen[[1]], sen[[2]], sen[[3]])
+plot(sen2)
+pairs(sen2)
+sample <- sampleRandom(sen2, 10000)
+sample
+pca <- prcomp(sample)
+pca
+summary(pca)
+plot(pca)
+pca
+pci <- predict(sen2, pca, index=c(1:2))
+pci
+plot(pci[[1]])
+
+
+library(ggplot2)
+library(viridis)
+ggplot() +
+geom_raster(pci, mapping = aes(x=x, y=y, fill=PC1)) +
+scale_fill_viridis()
+pcid <- as.data.frame(pci[[1]], xy=T)
+ggplot() +
+geom_raster(pcid, mapping = aes(x=x, y=y, fill=PC1)) +
+scale_fill_viridis()
+pcid <- as.data.frame(pci[[3]], xy=T)
+ggplot() +
+geom_raster(pcid, mapping = aes(x=x, y=y, fill=PC3)) +
+scale_fill_viridis()
+
+#patchwork
+library(patchwork)   # mettere le library tutte sopra 
+pci1 <- as.data.frame(pci[[1]], xy=T)
+pci3 <- as.data.frame(pci[[3]], xy=T)
+plot1 <- ggplot() +
+geom_raster(pci1, mapping = aes(x=x, y=y, fill=PC1)) +
+scale_fill_viridis()
+
+plot3 <- ggplot() +
+geom_raster(pci3, mapping = aes(x=x, y=y, fill=PC3)) +
+scale_fill_viridis()
+plot1 + plot3
+
+
+sd3d <- as.data.frame(sd3, xy=T)
+sd3 <- focal(pci[[1]], matrix(1/9, 3, 3), fun=sd) # deviazione standard 
+plotsd <- ggplot() +
+geom_raster(pci3, mapping = aes(x=x, y=y, fill=PC3)) +
+scale_fill_viridis(option=inferno) + 
+ggtitle("standard deviation of PC1 (3x3))
+
+
+
+
+
+
+
+
