5fc0b89c6bd02b3edb5510c36ecfa0176863736f
COSIMOMATTIA <102244225+COSIMOMATTIA@users.noreply.github.com>
2023-06-01 14:20:10 +0200

    Update R_code_Copernicus.R

--- R_code_Copernicus.R
+++ R_code_Copernicus.R
@@ -5,4 +5,17 @@ library(ncdf4)
 library(ggplot2)
 library(viridis)
 setwd("C:/lab/")
-#RICORDATI DI COMMENTARE PER IL Much :)
+
+ssoil <- raster("~/Downloads/c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc")
+
+ssoil
+
+plot(ssoil)
+
+ggplot() +
++ geom_raster(ssoil, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture))
+
+#viridis
+ggplot() +
+geom_raster(ssoil, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture)) +
+scale_fill_viridis(option="magma")
